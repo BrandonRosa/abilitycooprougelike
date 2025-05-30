2c4b9eee2cc7a0a8609ed01f280bc3bd6f71b790
BrandonRosa <bjbrandon11@gmail.com>
2025-05-30 17:09:27 -0500

    Doin shit i guess

--- Scripts/Modules/Projectile/BaseProjectile.cs
+++ Scripts/Modules/Projectile/BaseProjectile.cs
@@ -9,9 +9,9 @@ using static BrannPack.ModifiableStats.AbilityStats;
 
 namespace BrannPack.Projectile
 {
-    public class ProjectileSpawnInfo:EventInfo
+    public class ProjectileInfo:EventInfo
     {
-        public ProjectileSpawnInfo(CharacterMaster source, CharacterMaster destination, (int sourceType, int sourceIndex, int sourceEffect) key,
+        public ProjectileInfo(CharacterMaster source, CharacterMaster destination, (int sourceType, int sourceIndex, int sourceEffect) key,
              float damage, bool isCrit, Vector2 directionFrom = default, StatsHolder stats = null)
              : base(source, destination, key) =>
              (Damage, IsCrit, DirectionFrom, Stats) = (damage, isCrit, directionFrom, stats);
@@ -21,14 +21,25 @@ namespace BrannPack.Projectile
         public float Speed { get; set; } = 1000f;
         public float LifeTime { get; set; } = 5f;
         public float Range { get; set; } = 1000f;
+
+        public float Damage 
+
+        public int MaxCollisions = 1;
+
+        public StatsHolder
     }
     public interface IProjectile
     {
 
-        public void Initialize(ProjectileSpawnInfo projectileInfo);
+        public void Initialize(ProjectileInfo projectileInfo);
         public void Fire(Vector2 direction, Vector2 position);
         public void Update(float delta);
         public void Destroy();
+
+        public List<Action<IProjectile>> OnCollision { get; set; }
+
+        public ProjectileInfo InitialProjectileInfo { get; set; }
+        public ProjectileInfo ProjectileInfo { get; set; }
     }
     public partial class BaseProjectile: Node2D, IProjectile
     {
